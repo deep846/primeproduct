494a2232096efdd79a787c4808682bc5d8c5530a
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-05 21:23:35 +0530

    uploded file

---
+++ primeproduct.py
@@ -0,0 +1,11 @@
+# Write a Python function primeproduct(m) that takes an integer m as input and returns True if m is a prime product and False otherwise. (If m is not positive, your function should return False.) 
+
+def primeproduct(m):
+    factor=[]
+    for i in range (1,m+1):
+        if(m==0):
+           factor.append(i)
+    if(len(factor)<=4):
+        print('True')
+    else:
+        print('False')
